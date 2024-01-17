4f6984b0e57e54d7f7512a9a4cff8bc6bb53acdd
asmi046 <asmi046@gmail.com>
2024-01-18 00:51:05 +0300

    +1

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,12 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/asmi_all.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/asmi_auth.php'));
         });
     }
 
