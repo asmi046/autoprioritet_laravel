169d2d8e7d31fe4ce18ce38c28fc739f9299a39c
asmi046 <asmi046@gmail.com>
2024-01-22 04:13:43 +0300

    Недавно искали

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,9 +10,36 @@
 
 @section('main')
 
-    <section class="main_page_section">
+    <section class="last_serch_section">
         <div class="container">
+            <h2 class="mt_0">Недавно искали</h2>
+            <div class="last_search_line">
+                <a href="{{route("search", ['search'=> "DSF066G"])}}">DSF066G</a>
+                <a href="{{route("search", ['search'=> "OP570"])}}">OP570</a>
+                <a href="{{route("search", ['search'=> "R155113"])}}">R155113</a>
+                <a href="{{route("search", ['search'=> "0470125"])}}">0470125</a>
+                <a href="{{route("search", ['search'=> "OE6746"])}}">OE6746</a>
+                <a href="{{route("search", ['search'=> "K035671XS"])}}">K035671XS</a>
+                <a href="{{route("search", ['search'=> "96554421"])}}">96554421</a>
+                <a href="{{route("search", ['search'=> "25927045"])}}">25927045</a>
+                <a href="{{route("search", ['search'=> "95102324"])}}">95102324</a>
+                <a href="{{route("search", ['search'=> "25927044"])}}">25927044</a>
+                <a href="{{route("search", ['search'=> "8973643860VDS"])}}">8973643860VDS</a>
+                <a href="{{route("search", ['search'=> "4780039410"])}}">4780039410</a>
+                <a href="{{route("search", ['search'=> "8973643860"])}}">8973643860</a>
+                <a href="{{route("search", ['search'=> "25927045"])}}">25927045</a>
+            </div>
+        </div>
+    </section>
 
+    <section class="materialse_section">
+        <div class="container">
+            <h2>Полезные материалы</h2>
+            <div class="materials_wrapper">
+                @for ($i=0; $i<6; $i++)
+                    <x-materials.card></x-materials.card>
+                @endfor
+            </div>
         </div>
     </section>
 
