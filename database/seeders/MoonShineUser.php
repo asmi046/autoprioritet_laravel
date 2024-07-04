b3ca6e0d3cef0f1e69d6a0f17abf22320f895f2a
asmi046 <asmi046@gmail.com>
2024-07-04 19:52:09 +0300

    Сид для MoonShine

---
+++ database/seeders/MoonShineUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MoonShineUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userID = DB::table("moonshine_users")->insertGetId(
+            [
+                    'name' => "Супер Админ",
+                    'email' => "asmi046@gmail.com",
+                    'password' => Hash::make("123"),
+                    'moonshine_user_role_id' => 1,
+                    'created_at' => date("Y-m-d H:i:s"),
+                    'updated_at' => date("Y-m-d H:i:s")
+            ]
+        );
+    }
+}
