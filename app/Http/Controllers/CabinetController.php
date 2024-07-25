5cf0b3c1b632ad170c8d7048c3a0d7a50a3c6df6
asmi046 <asmi046@gmail.com>
2024-07-25 23:00:24 +0300

    Запрос заказов клиента

--- app/Http/Controllers/CabinetController.php
+++ app/Http/Controllers/CabinetController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CabinetController extends Controller
 {
     public function index(Request $request) {
-        return view('cabinet.cabinet');
+        $orders = Order::where('user_id', Auth::user()->id)->paginate(15);
+
+        return view('cabinet.cabinet', ['orders' => $orders]);
     }
 
     public function user_data(Request $request) {
