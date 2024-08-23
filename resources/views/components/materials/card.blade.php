73c3a191a2f5a49690e7064b188532be0e90dedc
asmi046 <asmi046@gmail.com>
2024-08-23 14:56:25 +0300

    -blog

--- resources/views/components/materials/card.blade.php
+++ resources/views/components/materials/card.blade.php
@@ -1,6 +1,6 @@
 <div class="material_card">
     <div class="img_wrapper">
-        <img class="img-obj" src="{{ Storage::url('blog/'.$item->img) }}" alt="{{ $item->title }}">
+        <img class="img-obj" src="{{ Storage::url($item->img) }}" alt="{{ $item->title }}">
     </div>
     <div class="info_blk">
         <h2 class="mt_0">{{ $item->title }}</h2>
